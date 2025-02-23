7e505eeac9e458a238774e93b53e2713e8d091e9
jaimesHub <duongquockhanh230596@gmail.com>
2025-02-23 17:23:28 +0700

    store user login with react context

--- src/components/layout/app.header.tsx
+++ src/components/layout/app.header.tsx
@@ -1,7 +1,13 @@
+import { useCurrentApp } from "../context/app.context";
+
 const AppHeader = () => {
+    const { user } = useCurrentApp();
     return (
         <div>
             app header
+            <div>
+                {JSON.stringify(user)}
+            </div>
         </div>
     );
 }
