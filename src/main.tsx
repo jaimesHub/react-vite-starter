eea8b15cba3aeb203a0359a713b3de3c9fa15da0
jaimesHub <duongquockhanh230596@gmail.com>
2025-02-23 20:51:41 +0700

    protected pages/routes

--- src/main.tsx
+++ src/main.tsx
@@ -13,6 +13,7 @@ import 'styles/global.scss';
 import HomePage from 'pages/client/home';
 import { App } from 'antd';
 import { AppProvider } from 'components/context/app.context';
+import ProtectedRoute from 'components/auth';
 
 const router = createBrowserRouter([
   {
@@ -30,6 +31,22 @@ const router = createBrowserRouter([
       {
         path: "/about",
         element: <AboutPage />,
+      },
+      {
+        path: "/chat",
+        element: (
+          <ProtectedRoute>
+            <div>chat page</div>
+          </ProtectedRoute>
+        ),
+      },
+      {
+        path: "/admin",
+        element: (
+          <ProtectedRoute>
+            <div>admin page</div>
+          </ProtectedRoute>
+        ),
       },
     ]
   },
